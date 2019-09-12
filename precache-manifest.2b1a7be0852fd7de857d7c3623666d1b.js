0c90a15f0187b9115e2f04384c7c85fed5f9508a
Nilesh Patel <npatel@comscore.com>
2019-09-12 12:57:45 +0530

    Updates

---
+++ precache-manifest.2b1a7be0852fd7de857d7c3623666d1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea3b81a9aef355fe724ce818a680c09",
+    "url": "/cra-contact-list/index.html"
+  },
+  {
+    "revision": "f3fc6a0b3418c693a4d9",
+    "url": "/cra-contact-list/static/css/main.ae9f0881.chunk.css"
+  },
+  {
+    "revision": "dda5aa4c229e111f2aef",
+    "url": "/cra-contact-list/static/js/2.6b0475e3.chunk.js"
+  },
+  {
+    "revision": "f3fc6a0b3418c693a4d9",
+    "url": "/cra-contact-list/static/js/main.327c213b.chunk.js"
+  },
+  {
+    "revision": "d618098e02b8597df9f6",
+    "url": "/cra-contact-list/static/js/runtime~main.ade4ede3.js"
+  }
+]);
