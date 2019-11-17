d8b16d3d17136901b08809923622a317bd803897
Nilesh Patel <nilesh.nvs@hotmail.com>
2019-11-17 23:18:23 +0530

    test

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,7 +9,7 @@ export const Header: React.SFC<IHeader> = (props: IHeader) => {
     <nav className="flex items-center justify-between flex-wrap bg-blue-500 p-4">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <span className="font-semibold text-xl tracking-tight">
-          Simple Contact App
+          Simple Contact App -first PR
         </span>
       </div>
       <div className="w-full block flex flex-row-reverse lg:items-center lg:w-auto">
